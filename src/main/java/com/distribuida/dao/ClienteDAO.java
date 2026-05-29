08649a100ed1cc23cf58473257406b5a64a6ad09
Erick <eguevara@tecnologicoismac.edu.ec>
2026-05-28 19:36:02 -0500

    Mi primer versionamiento

---
+++ src/main/java/com/distribuida/dao/ClienteDAO.java
@@ -0,0 +1,13 @@
+package com.distribuida.dao;
+
+import com.distribuida.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteDAO extends JpaRepository<Cliente, Integer> {
+
+    //Holii mi nombre es Erick
+
+    //hola a todos
+
+
+}
